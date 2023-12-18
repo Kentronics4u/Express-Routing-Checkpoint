46b34a21293858464919edb4acd5d53c7a3a3ee9
Kentronics <kentronics4u@gmail.com>
2023-12-18 15:15:27 +0100

    made some corrections

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Custom middleware to verify the time of the request
 const checkWorkingHours = (req, res, next) => {
